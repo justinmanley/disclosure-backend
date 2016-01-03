c22cc6ee14d0c36a44a2b56b594ded6eb96484c2
Justin Manley <manleyjster@gmail.com>
2016-01-03 03:28:51 +0000

    Nullify empty values in processed donors table for dedupe.

--- donors.sql
+++ donors.sql
@@ -26,3 +26,19 @@ FROM S497_CD;
 ALTER TABLE donors
     ADD COLUMN donor_id INT NOT NULL AUTO_INCREMENT FIRST,
     ADD PRIMARY KEY (donor_id);
+-- Dedupe requires missing values to be represented by NULL, rather than the empty string.
+-- Allows the listed columns to contain NULL values.
+ALTER TABLE donors
+    MODIFY name VARCHAR(268) NULL,
+    MODIFY occupation VARCHAR(60) NULL,
+    MODIFY employer VARCHAR(200) NULL,
+    MODIFY address VARCHAR(44) NULL,
+    MODIFY committee_id VARCHAR(9) NULL;
+UPDATE donors
+SET
+    -- Nullify empty strings for dedupe.
+    name = CASE name WHEN '' THEN NULL ELSE name END,
+    occupation = CASE occupation WHEN '' THEN NULL ELSE occupation END,
+    employer = CASE employer WHEN '' THEN NULL ELSE employer END,
+    address = CASE address WHEN '' THEN NULL ELSE address END,
+    committee_id = CASE committee_id WHEN '' THEN NULL ELSE committee_id END;  
